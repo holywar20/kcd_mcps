784d2a7b4bb5a369b348af41fd5362e60c49da04
Bryan Winter <bwinter@AM.IDTCORP.NET>
2026-07-22 16:54:06 -0400

    sync: LT-DELL54qh774 2026-07-22T20:54:00.853Z

--- starmind_kcd/src/guards/index.ts
+++
@@ -1,4 +0,0 @@
-export { AbstractGuard, GuardError } from './AbstractGuard';
-export type { ToolRequest } from './AbstractGuard';
-export { GuardChain } from './GuardChain';
-export { PathGuard } from './PathGuard';
--- starmind_kcd/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { DaedalusServer } from './server';
-
-new DaedalusServer().run().catch( ( err ) => {
-	process.stderr.write( `daedalus-mcp: fatal: ${ err }\n` );
-	process.exit( 1 );
-} );
